d8d0cbac5ef8c56fd23f7172428fa61430b1302b
mariorafael21 <mario.rafael99@outlook.com>
2022-04-18 23:02:55 -0300

    crud em PHP usando a clase PDO

---
+++ pdo.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Projeto de aplicação CRUD utilizando PDO - Agenda de Contatos
+ *
+ * Alexandre Bezerra Barbosa
+ */
+ 
+// Verificar se foi enviando dados via POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (isset($_POST["id"]) && $_POST["id"] != null) ? $_POST["id"] : "";
+    $nome = (isset($_POST["nome"]) && $_POST["nome"] != null) ? $_POST["nome"] : "";
+    $email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
+    $celular = (isset($_POST["celular"]) && $_POST["celular"] != null) ? $_POST["celular"] : NULL;
+} else if (!isset($id)) {
+    // Se não se não foi setado nenhum valor para variável $id
+    $id = (isset($_GET["id"]) && $_GET["id"] != null) ? $_GET["id"] : "";
+    $nome = NULL;
+    $email = NULL;
+    $celular = NULL;
+}
+ 
+// Cria a conexão com o banco de dados
+try {
+    $conexao = new PDO("mysql:host=localhost;dbname=crudsimples", "root", "");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->exec("set names utf8");
+} catch (PDOException $erro) {
+    echo "Erro na conexão:".$erro->getMessage();
+}
+ 
+// Bloco If que Salva os dados no Banco - atua como Create e Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "save" && $nome != "") {
+    try {
+        if ($id != "") {
+            $stmt = $conexao->prepare("UPDATE contatos SET nome=?, email=?, celular=? WHERE id = ?");
+            $stmt->bindParam(4, $id);
+        } else {
+            $stmt = $conexao->prepare("INSERT INTO contatos (nome, email, celular) VALUES (?, ?, ?)");
+        }
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $email);
+        $stmt->bindParam(3, $celular);
+ 
+        if ($stmt->execute()) {
+            if ($stmt->rowCount() > 0) {
+               ?>
+               <div class="alert alert-success" role="alert">
+                    Cadastro feito com sucesso!
+                </div>
+               <?php
+                $id = null;
+                $nome = null;
+                $email = null;
+                $celular = null;
+            } else {
+                echo "Erro ao tentar efetivar cadastro";
+            }
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if que recupera as informações no formulário, etapa utilizada pelo Update
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "upd" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("SELECT * FROM contatos WHERE id = ? ORDER BY id desc");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_OBJ);
+            $id = $rs->id;
+            $nome = $rs->nome;
+            $email = $rs->email;
+            $celular = $rs->celular;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+ 
+// Bloco if utilizado pela etapa Delete
+if (isset($_REQUEST["act"]) && $_REQUEST["act"] == "del" && $id != "") {
+    try {
+        $stmt = $conexao->prepare("DELETE FROM contatos WHERE id = ?");
+        $stmt->bindParam(1, $id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            ?>
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </symbol>
+            </svg>
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                <div>
+                    Cadastro exluído com sucesso!
+                </div>
+            </div>
+            <?php
+            $id = null;
+        } else {
+            throw new PDOException("Erro: Não foi possível executar a declaração sql");
+        }
+    } catch (PDOException $erro) {
+        echo "Erro: ".$erro->getMessage();
+    }
+}
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+            <title>Agenda de contatos</title>
+        </head>
+        <body>
+            <form action="?act=save" method="POST" name="form1" >
+            <h1 class="display-5">CADASTRO DE USUÁRIOS</h1>
+                <hr>
+                <input type="hidden" name="id" <?php
+                 
+                // Preenche o id no campo id com um valor "value"
+                if (isset($id) && $id != null || $id != "") {
+                    echo "value=\"{$id}\"";
+                }
+                ?> />
+                Nome:
+               <input type="text" name="nome" <?php
+ 
+               // Preenche o nome no campo nome com um valor "value"
+               if (isset($nome) && $nome != null || $nome != "") {
+                   echo "value=\"{$nome}\"";
+               }
+               ?> />
+               E-mail:
+               <input type="text" name="email" <?php
+ 
+               // Preenche o email no campo email com um valor "value"
+               if (isset($email) && $email != null || $email != "") {
+                   echo "value=\"{$email}\"";
+               }
+               ?> />
+               Celular:
+               <input type="text" name="celular" <?php
+ 
+               // Preenche o celular no campo celular com um valor "value"
+               if (isset($celular) && $celular != null || $celular != "") {
+                   echo "value=\"{$celular}\"";
+               }
+               ?> />
+               <button type="submit" class="btn btn-dark">SALVAR</button>
+               <button type="reset" class="btn btn-dark">LIMPAR</button>
+               <hr>
+            </form>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">E-MAIL</th>
+                    <th scope="col">CELULAR</th>
+                    <th scope="col">AÇÕES</th>
+                    </tr>
+                </thead>
+                <?php
+ 
+                // Bloco que realiza o papel do Read - recupera os dados e apresenta na tela
+                try {
+                    $stmt = $conexao->prepare("SELECT * FROM contatos");
+                    if ($stmt->execute()) {
+                        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+                            echo "<tr>";
+                            echo "<td>".$rs->id."</td><td>".$rs->nome."</td><td>".$rs->email."</td><td>".$rs->celular
+                                       ."</td><td><center><a href=\"?act=upd&id=".$rs->id."\">[Alterar]</a>"
+                                       ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+                                       ."<a href=\"?act=del&id=".$rs->id."\">[Excluir]</a></center></td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "Erro: Não foi possível recuperar os dados do banco de dados";
+                    }
+                } catch (PDOException $erro) {
+                    echo "Erro: ".$erro->getMessage();
+                }
+                ?>
+            </table>
+<footer class="text-center text-lg-start bg-light text-muted">
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <div class="me-5 d-none d-lg-block">
+      <span></span>
+    </div>
+  </section>
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Tecnologias usadas
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">PHP</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">HMTL</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Bootstrap</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contato
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Natal-RN, BR</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            mario.rafael99@outlook.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> 84 99665-9747</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://github.com/mariorafael21">Mário Rafael</a>
+  </div>
+</footer>
+        </body>
+    </html>
